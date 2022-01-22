89b522901ff0368f418e656597e46e6cf47575b1
vvlmvska <vvilimovska@gmail.com>
2022-01-22 14:44:19 +0100

    1 file modified, 1 added

--- index.js
+++
@@ -1,7 +0,0 @@
-let cislo = Number(prompt('Zadej:')); 
-let soucet = 0;
-while (cislo != 0) {
-soucet = soucet + cislo; 
-cislo = Number(prompt('Zadej:'));
-console.log('Naakumulovaný součet:' + soucet); 
-}
---
+++ ukol cykly.js
@@ -0,0 +1,7 @@
+let cislo = Number(prompt('Zadej:')); 
+let soucet = 0;
+while (cislo != 0) {
+soucet = soucet + cislo; 
+cislo = Number(prompt('Zadej:'));
+console.log('Naakumulovaný součet:' + soucet); 
+}
